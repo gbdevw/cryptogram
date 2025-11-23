8dcdb67b2a679d44fed317f30e7446998838ef99
gbdevw <guillaume.braibant@lake42.eu>
2025-11-23 13:26:55 +0100

    feat: Export web3pgp functions in index.ts

--- sdks/typescript/src/index.ts
+++ sdks/typescript/src/index.ts
@@ -1,8 +1,12 @@
 // Export utilities
 export * from './utils/0xstr';
 
-// TODO: Export web3pgp functions when implemented
-// export * from './web3pgp';
+// Export web3pgp functions
+export * from './web3pgp/types/types';
+export * from './web3pgp/web3pgp.interface';
+export * from './web3pgp/web3pgp.service.interface';
+export * from './web3pgp/web3pgp';
+export * from './web3pgp/web3pgp.service';
 
 // TODO: Export web3doc functions when implemented
 // export * from './web3doc';
