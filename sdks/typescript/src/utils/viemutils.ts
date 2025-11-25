6b3a6ac09c48d786e246385965ecec2b5203bb86
gbdevw <guillaume.braibant@lake42.eu>
2025-11-25 15:58:04 +0100

    feat: Enhance documentation for getBlockTimestamp function

--- sdks/typescript/src/utils/viemutils.ts
+++ sdks/typescript/src/utils/viemutils.ts
@@ -1,5 +1,11 @@
 import { PublicClient } from 'viem';
 
+/**
+ * Gets the timestamp of a specific block number.
+ * @param client The Viem public client to interact with the blockchain.
+ * @param blockNumber The block number to get the timestamp for.
+ * @returns The timestamp of the block as a Date object.
+ */
 export async function getBlockTimestamp(client: PublicClient, blockNumber: bigint): Promise<Date> {
     const block = await client.getBlock({ blockNumber });
     return new Date(Number(block.timestamp) * 1000);
