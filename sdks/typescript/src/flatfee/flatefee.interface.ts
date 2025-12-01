0164e0d1f5e5d1e7ccf409a23672654452df25db
gbdevw <guillaume.braibant@lake42.eu>
2025-12-01 19:11:10 +0100

    refactor: Remove access control inheritance from FlatFee class and add address and client properties

--- sdks/typescript/src/flatfee/flatefee.interface.ts
+++ sdks/typescript/src/flatfee/flatefee.interface.ts
@@ -1,13 +1,10 @@
 import { Address, TransactionReceipt } from 'viem';
 import { RequestedFeeUpdatedLog, FeesWithdrawnLog } from './types/types';
-import { IAccessManaged } from '../accessmanaged/accessmanaged.interface';
 
 /**
  * Interface for flat fee management operations.
- * 
- * Extends IAccessManaged to include access control functionality.
  */
-export interface IFlatFee extends IAccessManaged {
+export interface IFlatFee {
 
     /*****************************************************************************************************************/
     /* WRITE FUNCTIONS                                                                                               */
