96c9942d09f8799c836b17b4d1a7bed9ad39d904
gbdevw <guillaume.braibant@lake42.eu>
2025-12-14 15:55:15 +0100

    remove possibly undefined logs args as strict mode will be enforced

--- sdks/typescript/src/web3pgp/types/types.ts
+++ sdks/typescript/src/web3pgp/types/types.ts
@@ -9,11 +9,11 @@ import { BaseLog } from "../../common/types/types";
  */
 export type KeyRegisteredLog =  BaseLog & {
     // The declared fingerprint of the registered key as bytes32 hex string
-    primaryKeyFingerprint: `0x${string}` | undefined;
+    primaryKeyFingerprint: `0x${string}`;
     // The declared fingerprints of any subkeys associated with the registered key as bytes32 hex strings
-    subkeyFingerprints: readonly `0x${string}`[] | undefined;
+    subkeyFingerprints: readonly `0x${string}`[];
     // Hex-encoded OpenPGP binary message published on-chain which should contain the public key data
-    openPGPMsg: `0x${string}` | undefined;
+    openPGPMsg: `0x${string}`;
 };
 
 /**
@@ -25,11 +25,11 @@ export type KeyRegisteredLog =  BaseLog & {
  */
 export type SubkeyAddedLog = BaseLog & {
     // The declared fingerprint of the primary key to which the subkey was added as bytes32 hex string
-    primaryKeyFingerprint: `0x${string}` | undefined;
+    primaryKeyFingerprint: `0x${string}`;
     // The declared fingerprint of the added subkey as bytes32 hex string
-    subkeyFingerprint: `0x${string}` | undefined;
+    subkeyFingerprint: `0x${string}`;
     // Hex-encoded OpenPGP binary message published on-chain which should contain the public key data
-    openPGPMsg: `0x${string}` | undefined;
+    openPGPMsg: `0x${string}`;
 };
 
 /**
@@ -41,7 +41,7 @@ export type SubkeyAddedLog = BaseLog & {
  */
 export type KeyRevokedLog =  BaseLog & {
     // The declared fingerprint of the revoked key as bytes32 hex string
-    fingerprint?: `0x${string}` | undefined;
+    fingerprint?: `0x${string}`;
     // Hex-encoded OpenPGP binary message published on-chain which should contain the revocation certificate
-    revocationCertificate?: `0x${string}` | undefined;
+    revocationCertificate?: `0x${string}`;
 };
