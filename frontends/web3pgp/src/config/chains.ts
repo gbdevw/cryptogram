f8128494381ccfe4fda04f6ef8d816334dc2cffc
gbdevw <guillaume.braibant@lake42.eu>
2026-02-12 13:59:51 +0100

    Fix Sepolia contract address in CHAIN_CONFIG

--- frontends/web3pgp/src/config/chains.ts
+++ frontends/web3pgp/src/config/chains.ts
@@ -4,7 +4,7 @@ export const CHAIN_CONFIG = {
     chainId: 11155111,
     displayName: '[DEMO] Sepolia',
     // Web3PGP contract address for key management and verification
-    web3pgpContractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_SEPOLIA || '0xDa63568866C8eB53627a5CCF27DaB76061538dB1',
+    web3pgpContractAddress: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS_SEPOLIA || '0x82733B49e65A2FE6B611e5CE454AC21237071638',
   },
   scrollSepolia: {
     id: 'scrollSepolia',
